9541cf0a6599d6161f182c4b93cafed4eef92bcb
David Baldeschwieler <db@graphicdeveloper.com>
2022-09-30 17:08:39 +0100

    Adds a couple new npm scripts to package.json, and refines .eslintconfig to relax a few rules (and comments out airbnb-base config - at least for now (I disagree with too many of their restrictions, lol).

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,49 +4,49 @@
 // Docs source: https://blog.appsignal.com/2022/01/19/how-to-set-up-a-nodejs-project-with-typescript.html
 // Docs source (re: Prettier config - see bottom): https://typescript-eslint.io/docs/linting/configs
 module.exports = {
-	root: true,
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "2021",
-		sourceType: "module",
-		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-	},
-	plugins: ["@typescript-eslint"],
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-    "airbnb-base",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-		"prettier",
-	],
-	env: {
-		node: true, 	// Enable Node.js global variables
-		es2021: true
-	},
-	ignorePatterns: [
-		"jest.config.ts",
-		"dist",
-		"node_modules"
-	],
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "2021",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    // "airbnb-base",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  env: {
+    node: true, // Enable Node.js global variables
+    es2021: true,
+  },
+  ignorePatterns: ["jest.config.ts", "dist", "node_modules"],
   settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [".ts", ".d.ts", ".js"]
-      }
-    }
+      node: {
+        extensions: [".ts", ".d.ts", ".js"],
+      },
+    },
   },
-	// ESLint rules: https://eslint.org/docs/latest/rules/
-	// @typescript-eslint rules: https://typescript-eslint.io/rules/
+  // ESLint rules: https://eslint.org/docs/latest/rules/
+  // @typescript-eslint rules: https://typescript-eslint.io/rules/
   // @airbnb rules: https://airbnb.io/javascript/
-	rules: {
+  rules: {
     "no-console": "off",
     "no-plusplus": "off",
+    "no-useless-concat": "off",
     "import/extensions": "off",
-		"prefer-const": "warn",
+    "prefer-const": "warn",
+    "prefer-template": "warn",
     "no-unused-vars": "warn",
-		"@typescript-eslint/no-inferrable-types": "off",
-		"@typescript-eslint/restrict-plus-operands": "warn",
+    // "import/prefer-default-export": "warn",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/restrict-plus-operands": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
-	}
+  },
 };
